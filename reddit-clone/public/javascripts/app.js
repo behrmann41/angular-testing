eb609a82878632cff6831b635044a56b56b498a4
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-07 15:50:52 -0700

    users can add new post - posts formated in separate divs

--- reddit-clone/public/javascripts/app.js
+++ reddit-clone/public/javascripts/app.js
@@ -1,7 +1,24 @@
 var app = angular.module('redditClone', []);
 app.controller('CloneController', function($scope){
   $scope.showForm = false
-  $scope.submitPost = function (){
+  $scope.posts = [{ title: "Outside Aspen, CO", author: "Ned Stark",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae accumsan nunc. Vivamus eget metus eu urna imperdiet malesuada. Quisque varius elit sem. Phasellus elementum, risus vitae dapibus dictum, turpis dui interdum nisi, ut iaculis turpis odio ac ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                    imageUrl: "http://uniacke.novascotia.ca/sites/default/files/inline/images/uniacke-estate-trail.jpg"},
+                  { title: "Cliffs of Moher", author: "Bob Wellington",
+                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat tortor quam, eget cursus neque vehicula ut. Cras scelerisque iaculis.",
+                    imageUrl: "http://www.uppercrustindia.com/dynamic/uploads/image/UC_57/Travel/Ireland%20Harbour%20Flight/Ariel-Shot-Of-Cliffs-Of-Moher.jpg"
+                  }]
+  $scope.postForm = function (){
     $scope.showForm = ($scope.showForm === true) ? false : true
+  }
+  $scope.submitPost = function (){
+    $scope.newPost = {  title: $scope.newPost.title,
+                        author: $scope.newPost.author,
+                        description: $scope.newPost.description,
+                        imageUrl: $scope.newPost.imageUrl
+                      }
+    $scope.posts.push($scope.newPost)
+    $scope.newPost = {}
+    $scope.showForm = false
   }
 })
