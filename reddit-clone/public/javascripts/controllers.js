af33415874425cee54a72ffcae7711cd8ec78a90
Ian Behrmann <ianjbehrmann@gmail.com>
2015-11-11 19:35:45 -0700

    added moment for time on reddit clone

--- reddit-clone/public/javascripts/controllers.js
+++ reddit-clone/public/javascripts/controllers.js
@@ -4,14 +4,14 @@ app.controller('CloneController', function($scope){
   $scope.thisComment = false
   $scope.posts = [{ title: "Outside Aspen, CO", author: "Ned Stark",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae accumsan nunc. Vivamus eget metus eu urna imperdiet malesuada. Quisque varius elit sem. Phasellus elementum, risus vitae dapibus dictum, turpis dui interdum nisi, ut iaculis turpis odio ac ligula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-                    imageUrl: "http://uniacke.novascotia.ca/sites/default/files/inline/images/uniacke-estate-trail.jpg", comments: [{commentAuthor: "Ashley Owens", message: "Wow what an amazing picture!"}, {commentAuthor: "Michael Snuttjer", message: "I think it is super average"}], votes: 0},
+                    imageUrl: "http://uniacke.novascotia.ca/sites/default/files/inline/images/uniacke-estate-trail.jpg", comments: [{commentAuthor: "Ashley Owens", message: "Wow what an amazing picture!"}, {commentAuthor: "Michael Snuttjer", message: "I think it is super average"}],time: 'Wed Nov 11 2015 19:04:55 GMT-0700 (MST)', votes: 0},
                   { title: "Cliffs of Moher", author: "Bob Wellington",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat tortor quam, eget cursus neque vehicula ut. Cras scelerisque iaculis.",
-                    imageUrl: "http://www.uppercrustindia.com/dynamic/uploads/image/UC_57/Travel/Ireland%20Harbour%20Flight/Ariel-Shot-Of-Cliffs-Of-Moher.jpg", comments: [{commentAuthor: "Chelsea Schnusttjer", message: "I think it is super average"}], votes: 3
+                    imageUrl: "http://www.uppercrustindia.com/dynamic/uploads/image/UC_57/Travel/Ireland%20Harbour%20Flight/Ariel-Shot-Of-Cliffs-Of-Moher.jpg", comments: [{commentAuthor: "Chelsea Schnusttjer", message: "I think it is super average"}],time: 'Tue Nov 10 2015 19:04:55 GMT-0700 (MST)', votes: 3
                   },
                   { title: "The best dog", author: "Duncan Donuts",
                     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec placerat tortor quam, eget cursus neque vehicula ut. Cras scelerisque iaculis.",
-                    imageUrl: "http://images8.alphacoders.com/441/441525.jpg", comments: [{commentAuthor: "Chelsea Schnusttjer", message: "I think he is super smart"}], votes: 7}
+                    imageUrl: "http://images8.alphacoders.com/441/441525.jpg", comments: [{commentAuthor: "Chelsea Schnusttjer", message: "I think he is super smart"}], time: 'Mon Nov 9 2015 19:04:55 GMT-0700 (MST)', votes: 7}
                   ]
   $scope.postForm = function (){
     $scope.showForm = ($scope.showForm === true) ? false : true
@@ -28,6 +28,7 @@ app.controller('CloneController', function($scope){
                           author: this.author,
                           description: this.description,
                           imageUrl: this.imageUrl,
+                          time: new Date(),
                           comments: [],
                           votes: 0
                         }
